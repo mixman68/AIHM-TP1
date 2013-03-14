07a1ca6379fe11d2357d788fce497892272272aa
Greg <greg.djg13@gmail.com>
2013-03-14 11:47:28 +0100

    Slider done with painted componant

--- src/aihm/tp1/RightPanel.java
+++ src/aihm/tp1/RightPanel.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 /**
  *
@@ -16,8 +18,10 @@ import javax.swing.*;
  */
 public class RightPanel extends JPanel {
 
-    JSlider slider;
-    JPanel child;
+    protected JSlider slider;
+    protected JPanel child;
+    
+    protected int sliderMin,sliderMax;
 
     RightPanel() {
         init();
@@ -25,6 +29,9 @@ public class RightPanel extends JPanel {
     }
     
     private void init() {
+        sliderMin = 0;
+        sliderMax = 100;
+        
         this.child = new ChildPanel();
         this.setLayout(new BorderLayout());
 
@@ -32,8 +39,20 @@ public class RightPanel extends JPanel {
     }
 
     private void initSlider() {
-        slider = new JSlider(JSlider.VERTICAL);
+        slider = new JSlider(JSlider.VERTICAL,sliderMin,sliderMax,sliderMin);
         this.add(slider,BorderLayout.EAST);
+        slider.addChangeListener(new SliderListener());
+    }
+    
+    class SliderListener implements ChangeListener
+    {
+
+        @Override
+        public void stateChanged(ChangeEvent e) {
+            JSlider slider = (JSlider) e.getSource();
+            child.repaint();
+        }
+        
     }
     
     class ChildPanel extends JPanel {
@@ -45,14 +64,25 @@ public class RightPanel extends JPanel {
         }
         @Override
         public void paintComponent(Graphics g){
+            int value = slider.getValue();
+            
+            
+            int yrect = (this.getHeight()-100)*(sliderMax-value)/sliderMax;
             super.paintComponent(g);
             g.setColor(Color.black);
             //g.drawRect(0,0,50,50);
             g.drawLine(60,10, 60, this.getHeight());
             g.drawLine(150, 10, 150, this.getHeight());
-            g.drawRect(65, this.getHeight(), 80, -100);
+            g.drawRect(65, yrect, 80, 99);
             g.setColor(Color.cyan);
-            g.fillRect(66, this.getHeight()-1, 79, -97);
+            g.fillRect(66, yrect+1, 79, 97);
+            
+            g.setColor(Color.black);
+            
+            StringBuffer buff = new StringBuffer();
+            buff.append(value);
+            buff.append(" %");
+            g.drawString(buff.toString(), 100, yrect+20);
         }
     }
 }
